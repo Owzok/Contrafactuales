90c745d0672d31ffce81c6b127af7b0c4eaa5177
Owzok <martin.perezbonany@utec.edu.pe>
2023-10-12 10:12:48 -0500

    sipi

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import json
 
 clf = joblib.load("Fixed_model.pkl")
 df = pd.read_csv("Data_limpia2.csv")
+df.replace({False: 0}, inplace=True)
+df.replace({True: 1}, inplace=True)
 
 d = dice_ml.Data(dataframe=df, continuous_features=['PONDERADO', 'IDH', 'HRS_FALTA_RATIO', 'POR_POBREZA', 'POR_POBREZA_EXTREMA', 'Tasa_Analfabetismo', 'Tasa_Desempleo2019'], outcome_name='ESTADO')
 
@@ -18,6 +20,18 @@ dropouts_list_index = df[df.ESTADO == -1].index.to_list()
 df.drop(["ESTADO"], axis=1, inplace=True)
 
 app = Flask(__name__)
+
+def find_changed_columns(test_data, cfs_list):
+    changed_columns = []
+    test_data_row = test_data.iloc[0]
+    cfs_list_row = cfs_list.iloc[0]
+
+    for col_name, test_value, cfs_value in zip(test_data.columns, test_data_row, cfs_list_row):
+        print(col_name, test_value, cfs_value)
+        if round(float(test_value),2) != round(float(cfs_value),2):
+            changed_columns.append(col_name)
+
+    return changed_columns
 
 def custom_counterfactuals(x):
     semestre_actual = x.SEM_CURSADOS.values[0]
@@ -43,13 +57,13 @@ def generate_random_counterfactual():
             counterfactuals = custom_counterfactuals(random_query)
 
             formatted_json = json.loads(counterfactuals.to_json())
-            print(json.loads(counterfactuals.to_json()))
             test_data = pd.DataFrame(formatted_json["test_data"][0], columns=formatted_json["feature_names_including_target"])
             cfs_list = pd.DataFrame(formatted_json["cfs_list"][0], columns=formatted_json["feature_names_including_target"])
             cfs_list.columns = cfs_list.columns.str.replace(' ', '_')
 
-            print(cfs_list)
-            return render_template("result.html", counterfactuals=formatted_json, test_data=test_data, cfs_list=cfs_list)
+            changed_columns = find_changed_columns(test_data, cfs_list)
+
+            return render_template("result.html", counterfactuals=formatted_json, test_data=test_data, cfs_list=cfs_list, changed_columns=changed_columns)
         except Exception as e:
             print(e)
 
@@ -88,7 +102,9 @@ def index():
                 cfs_list = pd.DataFrame(formatted_json["cfs_list"][0], columns=formatted_json["feature_names_including_target"])
                 cfs_list.columns = cfs_list.columns.str.replace(' ', '_')
 
-                return render_template("result.html", counterfactuals=counterfactuals, test_data=test_data, cfs_list=cfs_list)
+                changed_columns = find_changed_columns(test_data, cfs_list)
+
+                return render_template("result.html", counterfactuals=counterfactuals, test_data=test_data, cfs_list=cfs_list, changed_columns=changed_columns)
         except ValueError as e:
             error_message = str(e)
             print(e)
